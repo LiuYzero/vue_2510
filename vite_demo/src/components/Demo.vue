b447e25f124d0f7918ede1f43dbe268456c099e5
liuayng_msi5600g <liuyang_zero@outlook.com>
2025-10-09 20:45:23 +0800

    add .gitignore

---
+++ vite_demo/src/components/Demo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="demo">Demo 组件</div>
+    <div class="redline">Message : {{ message }}</div>
+    <div class="normalline">{{ name }}</div>
+    <div class="normalline">{{  obj.message  }}</div>
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+    const message = ref('Hello from Demo component!');
+    let name = ref("Vue 3 Data");
+    const obj = ref({
+        message:"欲穷千里目，更上一层楼。",
+    })
+
+    setTimeout(() => {
+        console.log("before message")
+        message.value = "Message Updated after 2 seconds!"
+        obj.value.message = "不畏浮云遮望远，自缘身在最高层。"
+        console.log("message")
+    }, 2000);
+</script>
+
+
+<style>
+.demo {
+    font-size: 22px;
+    font-weight: bold;
+}
+.redline {
+    color: red;
+}
+.normalline{
+    color: black;
+    font-size: 12px;
+}
+</style>
+
+export default {
+    name: 'Demo';
+}
---
+++ vite_demo/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+
+import Demo from './components/Demo.vue'
+
+createApp(Demo).mount('#app')
