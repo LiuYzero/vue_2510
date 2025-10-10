db6aea92de09b9c650a9eab8ac2bb16f8c9510d5
liuayng_msi5600g <liuyang_zero@outlook.com>
2025-10-10 22:56:22 +0800

    v-html v-text v-bind

---
+++ vite_confitional_rendering/src/components/ConditionalTest.vue
@@ -0,0 +1,21 @@
+<template>
+    <hr/>
+    <div v-text="message"></div>
+    <hr/>
+    <div v-html="html_message"></div>
+    <hr/>
+    <p>username: <input type="text" v-bind:placeholder="username"></p>
+    <p>password: <input type="password" v-bind:placeholder="password"></p>
+    <hr/>
+</template>
+
+<script setup>
+    const message = "<span>这里是v-text指令的使用</span>, First Time"
+    const html_message = "<strong>这里是v-html指令的使用 strong line</strong>"
+    const username = "请输入用户名"
+    const password = "请输入密码"
+</script>
+
+export default {
+    name: "ConditionalTest"
+}
---
+++ vite_confitional_rendering/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import ConditionalTest from './components/ConditionalTest.vue'
+
+createApp(ConditionalTest).mount('#app')
